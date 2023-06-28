53c0b3399f958cb3d6146d3c5c839acb2de044d0
Ilucha <pozta_01@mail.ru>
2023-06-28 22:52:07 +0300

    Win

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
-    }
-}
+        int mirror = 0;
+        while (number != 0) {
+            mirror = mirror * 10 + (number % 10);
+            number = number / 10;
+        }
+        System.out.println(mirror);
+    }}
